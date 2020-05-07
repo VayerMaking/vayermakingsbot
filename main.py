5c10217bafd37744162604d746e798d678162bb2
VayerM8king <martin.vayer@gmail.com>
2020-05-07 15:50:54 +0300

    updated bot activity

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = commands.Bot(command_prefix = '$')
 initial_extensions = ['cogs.basic','cogs.minecraft','cogs.reactions']
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Destruction Happening"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="хората в KurKuma"))
 if __name__ == '__main__':
     for extension in initial_extensions:
         bot.load_extension(extension)
